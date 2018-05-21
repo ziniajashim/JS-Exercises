10cd212fcc27501e7838249b7d9f542eb09566df
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-05-21 04:40:46 +0000

    task 2

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /* JavaScript code here */
 console.log("External JS")
+//task 2
+function sumArray(arr){
+    var sum = 0;
+    arr.foreach(function(value,index){
+        sum += value;
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4]));
+console.log(sumArray([1,20,-3,45]));
+ 
+
