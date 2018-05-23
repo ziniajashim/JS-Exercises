ef8d9102ae7f2656ea356cc94ed7aba73c4e0a64
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-05-23 03:48:47 +0000

    task 4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* JavaScript code here */
-console.log("External JS")
+/*console.log("External JS")
 //task 2
 function sumArray(arr){
     var sum = 0;
@@ -18,5 +18,27 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("johnsmith@gmail.com"));
- 
+ */
 
+//task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+function getReadingStatus(index){
+    return library[index].readingStatus;
+}
+console.log(getReadingStatus(1));
