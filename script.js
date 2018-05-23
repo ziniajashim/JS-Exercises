e4154456e8ecf324a115386dcccab4cd8d98135c
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-05-23 04:14:25 +0000

    task 5

--- script.js
+++ script.js
@@ -42,3 +42,26 @@ function getReadingStatus(index){
     return library[index].readingStatus;
 }
 console.log(getReadingStatus(1));
+//task 5
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem = {name:'Watch',price:64,quantity:1};
+addItem(newItem);
